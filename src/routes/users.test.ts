1981f1e557ebbbdb3350ce5b159bf0443d50b3a9
Paul Scott <32460795+LijahTheDev@users.noreply.github.com>
2021-10-30 19:34:45 -0500

    Disabled Test Command

--- src/routes/users.test.ts
+++ src/routes/users.test.ts
@@ -1,40 +1,40 @@
-// import request from 'supertest'
-// import Server from '../server'
-// import { runSeeder, tearDownDatabase, useRefreshDatabase, useSeeding } from 'typeorm-seeding';
-// import AppSeeder from '../data/seed/seeds/Seeder';
+import request from 'supertest'
+import Server from '../server'
+import { runSeeder, tearDownDatabase, useRefreshDatabase, useSeeding } from 'typeorm-seeding';
+import AppSeeder from '../data/seed/seeds/Seeder';
 
-// // @NOTE: For integration tests use a real database similar to production database
+// @NOTE: For integration tests use a real database similar to production database
 
-// const server = new Server();
+const server = new Server();
 
-// process.env.CONNECTION = 'testing'
+process.env.CONNECTION = 'testing'
 
-// beforeAll(async () => {
-//   await useRefreshDatabase({ connection: process.env.CONNECTION })
+beforeAll(async () => {
+  await useRefreshDatabase({ connection: process.env.CONNECTION })
 
-//   server.run(7001)
+  server.run(7001)
 
-//   await useSeeding()
-//   await runSeeder(AppSeeder)
-// });
+  await useSeeding()
+  await runSeeder(AppSeeder)
+});
 
-// afterAll(async () => {
-//   await tearDownDatabase()
-// });
+afterAll(async () => {
+  await tearDownDatabase()
+});
 
-// describe('GET /api/users', () => {
-//   test('responds with json', (done) => {
-//     try {
+describe('GET /api/users', () => {
+  test('responds with json', (done) => {
+    try {
 
-//       request(server.app)
-//         .get('/api/users')
-//         .expect((res) => console.log(res.body))
-//         .expect(200, done)
+      request(server.app)
+        .get('/api/users')
+        .expect((res) => console.log(res.body))
+        .expect(200, done)
 
-//     } catch (error) {
-//       done.fail(error)
-//     }
-//   });
-// });
+    } catch (error) {
+      done.fail(error)
+    }
+  });
+});
 
 
